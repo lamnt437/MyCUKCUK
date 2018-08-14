41666cb4e432761a8fc4dd60f88f46878a0ab6e8
Lam Nguyen <lamnt437@gmail.com>
2018-08-14 23:33:12 +0700

    test some new jquery tweaks

---
+++ MyCUKCUK/Script/MISAscript.js
@@ -0,0 +1,52 @@
+﻿$(".password-status").hide();
+$(".username-status").hide();
+$("#lang-selector").val("vietnamese");
+$("#lang-info").text($("#lang-selector").val());
+var $insertedElm = "<li><em>Hello, world</em></li>";
+
+$("#lang-selector").change(function () {
+    if ($("#lang-selector").val() === "english") {
+        $("#login-title").text("login cukcuk.vn");
+        $("#login-buttonID").val("LOGIN");
+    }
+    else if ($("#lang-selector").val() === "vietnamese") {
+        $("#login-title").text("đăng nhập cukcuk.vn");
+        $("#login-buttonID").val("ĐĂNG NHẬP");
+    }
+    else {
+        $("#login-title").text("einloggen cukcuk.vn");
+        $("#login-buttonID").val("EINLOGGEN");
+    }
+})
+
+$("#login-buttonID").click(function () {
+    $(".password-status").hide();
+    $(".username-status").hide();
+    //$("ul").prepend($insertedElm);
+    //$("#mid").css({
+    //    'background-color': 'black',
+    //    'font-family':'Courier'
+    //});
+    var username = $("#username-input").val();
+    var password = $("#password-input").val();
+
+    if (username.trim() === "") {
+        $(".username-status").show().text("Bạn phải nhập tên đăng nhập");
+    }
+    if (password.trim() === "") {
+        $(".password-status").show().text("Bạn phải nhập mật khẩu");
+    }
+    if (username.trim() !== "" && password.trim() !== "") {
+        if (username.trim() === "admin") {
+            if (password.trim() === "12345678") {
+                $(".username-status").show().text("Đăng nhập thành công!");
+            }
+            else {
+                $(".username-status").show().text("Mật khẩu sai");
+            }
+        }
+        else {
+            $(".username-status").show().text("Tên đăng nhập không tồn tại trong hệ thống!");
+        }
+    }
+})
